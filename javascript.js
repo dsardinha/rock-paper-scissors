c092aff36da04020e987739b05db110ea966da2f
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-12 16:45:30 +0100

    Implement getComputerChoice function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello World");
+const getComputerChoice = () => {
+    let computerChoice = Math.floor(Math.random() * 3);
+    if (computerChoice === 0) {
+        return "rock";
+    } else if (computerChoice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
