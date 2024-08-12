c8246f97b6fabcdea1505141831ff2fab7e23a14
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-12 16:56:19 +0100

    Implement getHumanChoice function

--- javascript.js
+++ javascript.js
@@ -1,10 +1,25 @@
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
-        return "rock";
+        return 'rock';
     } else if (computerChoice === 1) {
-        return "paper";
+        return 'paper';
     } else {
-        return "scissors";
+        return 'scissors';
+    }
+}
+
+const getHumanChoice = () => {
+    let humanChoice = prompt('Choose an option: "rock", "paper" or "scissors"')
+    .trim()
+    .toLowerCase();
+    if (humanChoice === 'rock' 
+        || humanChoice === 'paper' 
+        || humanChoice === 'scissors'
+        ) {
+        return humanChoice;
+    }
+    else {
+        alert('Invalid option. Please select "rock", "paper" or "scissors"');
     }
 }
