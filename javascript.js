e6b9e89c9fb8901e396008df04006992ba2f2180
dsardinha <15276275+dsardinha@users.noreply.github.com>
2024-08-12 17:02:00 +0100

    Declare score variables and options constants

--- javascript.js
+++ javascript.js
@@ -1,25 +1,32 @@
+const ROCK = 'rock';
+const PAPER = 'paper';
+const SCISSORS = 'scissors';
+
+let humanScore = 0;
+let computerScore = 0;
+
 const getComputerChoice = () => {
     let computerChoice = Math.floor(Math.random() * 3);
     if (computerChoice === 0) {
-        return 'rock';
+        return ROCK;
     } else if (computerChoice === 1) {
-        return 'paper';
+        return PAPER;
     } else {
-        return 'scissors';
+        return SCISSORS;
     }
 }
 
 const getHumanChoice = () => {
-    let humanChoice = prompt('Choose an option: "rock", "paper" or "scissors"')
+    let humanChoice = prompt(`Choose an option: "${ROCK}", "${PAPER}" or "${SCISSORS}"`)
     .trim()
     .toLowerCase();
-    if (humanChoice === 'rock' 
-        || humanChoice === 'paper' 
-        || humanChoice === 'scissors'
+    if (humanChoice === ROCK 
+        || humanChoice === PAPER 
+        || humanChoice === SCISSORS
         ) {
         return humanChoice;
     }
     else {
-        alert('Invalid option. Please select "rock", "paper" or "scissors"');
+        alert(`Invalid option. Please select "${ROCK}", "${PAPER}" or "${SCISSORS}"`);
     }
 }
